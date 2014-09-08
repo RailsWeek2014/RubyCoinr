bbbcbe9fa57a8d4faab98110d698ed3f27f3883a
Christian Schulze <christian.schulze@mni.thm.de>
2014-09-08 13:04:23 +0200

    added transaction helper for grouping transactions

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,4 +1,13 @@
 module TransactionsHelper
+
+	def clustered_txs_by_wallet
+		Transaction.all.to_a.cluster :wallet_id
+	end
+
+	def clustered_txs_by_status
+		Transaction.all.to_a.cluster :confirmed
+	end
+
 	def wallet_label_for_id id
 		Wallet.find(id).label
 	end
