e57fc566d713e84cb4d0edace9cc1747a8006730
Christian Schulze <christian.schulze@mni.thm.de>
2014-09-08 09:40:55 +0200

    closes #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_action :create_receiverqrcode
+
+  private
+  def create_receiverqrcode
+  	@receiverqrcode = Receiverqrcode.new
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
 	def index
-		# create new empty model for sidebar view
-		@receiverqrcode = Receiverqrcode.new
 	end
 end
