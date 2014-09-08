611f6901c534d947b72edde20e010e09ecf1116f
Christian Schulze <christian.schulze@mni.thm.de>
2014-09-08 11:07:43 +0200

    improved layout, styling and D.R.Y. regarding partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,51 @@ module ApplicationHelper
 
 		out
 	end
+
+	# TODO
+	def confirmed_incoming_txs_count
+		0
+	end
+
+	# TODO
+	def unconfirmed_incoming_txs_count
+		0
+	end
+
+	def confirmed_outgoing_txs_count
+		count = 0
+
+		if current_user.wallets.any?
+			current_user.wallets.each do |w|
+				if w.transactions.any?
+					w.transactions.each do |tx|
+						if tx.confirmed
+							count += 1
+						end
+					end
+				end
+			end
+		end
+
+		count
+
+	end
+
+	def unconfirmed_outgoing_txs_count
+		count = 0
+
+		if current_user.wallets.any?
+			current_user.wallets.each do |w|
+				if w.transactions.any?
+					w.transactions.each do |tx|
+						count += 1 unless tx.confirmed
+					end
+				end
+			end
+		end
+
+		count
+	end
+
 	
 end
