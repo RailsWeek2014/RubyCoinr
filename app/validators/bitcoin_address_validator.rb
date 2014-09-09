10ea016bcfdf25b0f85ede7ac926a3c71329e182
Christian Schulze <christian.schulze@mni.thm.de>
2014-09-09 14:32:06 +0200

    added sender_addr to validation if it exists; closes #5

--- app/validators/bitcoin_address_validator.rb
+++ app/validators/bitcoin_address_validator.rb
@@ -2,7 +2,13 @@ class BitcoinAddressValidator < ActiveModel::Validator
 
 	# validates bitcoin address
 	def validate(record)
+		# receiver address
 		record.errors.add(:receiver_addr, "invalid") unless Bitcoin::valid_address?(record.receiver_addr)
+
+		# sender address
+		if record.sender_addr
+			record.errors.add(:receiver_addr, "invalid") unless Bitcoin::valid_address?(record.sender_addr)
+		end
 	end
 
 end
